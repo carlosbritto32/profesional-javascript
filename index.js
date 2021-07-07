905ff69a5644477ab24c5ad2e7d650d5b3ddd58a
Carlos Britto <carlosbritto32@gmail.com>
2021-07-07 12:09:00 -0500

    la logica js la separe en un archivo index.js aparte

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-
-function mediaPlayer() {}
+function mediaPlayer(config) {
+  this.media = config.el;
+}
 
 mediaPlayer.prototype.play = function () {
   video.play();
 };
 
-const player = new mediaPlayer();
-button.onclick = () => video.play;
+mediaPlayer.prototype.pause = function () {
+  video.pause();
+};
+
+mediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+const video = document.querySelector("video");
+const player = new mediaPlayer({ el: video });
+
+const button = document.querySelector("button");
+
+button.onclick = () => player.togglePlay();
