e18953efe04f32b6f9eb5acb2d186a81b70fa5db
Carlos Britto <carlosbritto32@gmail.com>
2021-07-10 10:51:08 -0500

    ejericio de scope

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+import mediaPlayer from "./mediaPlayer";
+
+const video = document.querySelector("video");
+const player = new mediaPlayer({ el: video });
+
+const button = document.querySelector("button");
+button.onclick = () => player.togglePlay();
--- assets/mediaPlayer.js
+++ assets/mediaPlayer.js
@@ -18,9 +18,4 @@ mediaPlayer.prototype.togglePlay = function () {
   }
 };
 
-const video = document.querySelector("video");
-const player = new mediaPlayer({ el: video });
-
-const button = document.querySelector("button");
-
-button.onclick = () => player.togglePlay();
+export default mediaPlayer;
